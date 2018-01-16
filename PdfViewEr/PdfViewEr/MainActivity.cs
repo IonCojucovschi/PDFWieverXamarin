97b6d0c3070df2c5e92bec50556dc0d386cfdad2
Ion <i.cojucovschi@software-dep.net>
2018-01-16 16:58:08 +0200

    pdf view ,,,, trebuie sa fii atent cand adaogi librariile in aplicatie acestea trebuie de bifat ca librarii AndroidNativeLibrary

--- PdfViewEr/PdfViewEr/MainActivity.cs
+++ PdfViewEr/PdfViewEr/MainActivity.cs
@@ -1,18 +1,26 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Com.Joanzapata.Pdfview;
 
 namespace PdfViewEr
 {
     [Activity(Label = "PdfViewEr", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        PDFView pdfView;
+
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            pdfView = FindViewById<PDFView>(Resource.Id.pdfView);
+            pdfView.FromAsset("xamarin.pdf").Load();
+
         }
     }
 }
